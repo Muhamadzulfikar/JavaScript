c91b069f8d41ad110a228f9273ab5c3bfad3d32c
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2021-03-25 16:10:05 +0700

    Add files via upload

---
+++ Event/index.js
@@ -0,0 +1,9 @@
+function getname (){
+    document.getElementById('text').innerHTML = "Hallo saya sedang belajar js";
+}
+
+var input = document.getElementById('input');
+
+function getname2 () {
+    document.getElementById('text2').innerHTML = input.value;
+}

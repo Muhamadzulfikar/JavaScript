8425f4bff27fc0c1f42fc7389989a63e1395c321
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2022-03-03 19:13:49 +0800

    update repository

---
+++ array/index.js
@@ -0,0 +1,8 @@
+// const alterName = ['Muhamad', 'Zulfikar', 'Douma', 'Katsuragi', 'Ayu']
+
+// console.log(alterName) //print all array
+// console.log(alterName[2]) //print array spesifik
+// console.log(alterName.sort()) // print array with abjad(jika ada uppercase akan didahulukan daripada lowercase)
+// console.log(alterName.reverse()) //print array dari belakang
+// console.log(alterName.concat(['Yessi', 'Yesseh', 'Yohan'])) //menggabungkan array
+// console.log(alterName.join(" ")) //memisahkan array dengan separator
